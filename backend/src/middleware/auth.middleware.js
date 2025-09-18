1b00f5599b3a78c77f03c95714bb811da4e28f50
Aadi-0411 <aadityapatil2004@gmail.com>
2025-09-18 12:36:07 +0530

    Fixed Errors

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,19 +3,19 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req,res,next) => {
     try{
-        const token = req.cokkies.jwt
+        const token = req.cookies.jwt
 
         if(!token){
-            return req.status(401).json({message:"Unauthorized - No Token Provided"});
+            return res.status(401).json({message:"Unauthorized - No Token Provided"});
         }
 
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
 
         if(!decoded){
-            return req.status(401).json({message:"Unauthorized - Invalid Token"});
+            return res.status(401).json({message:"Unauthorized - Invalid Token"});
         }
 
-        const user = await User.findById(decoded.userID).select("-password");
+        const user = await User.findById(decoded.userId).select("-password");
 
         if(!user){
             return res.status(404).json({message:"User not found"});
