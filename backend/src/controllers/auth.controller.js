7f4519cac6080dd4cbd80ca57337abc6f896ff4c
Aadi-0411 <aadityapatil2004@gmail.com>
2025-09-18 12:27:11 +0530

    checkAuth route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -114,3 +114,12 @@ export const updateProfile = async(req,res) => {
     res.status(500).json({message:"Internal server error"});
   }
 };
+
+export const checkAuth = (req,res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status.json({message:"Internal Server Error"});
+  }
+};
