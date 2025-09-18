4c0dc8929c6dc4ed7f0c61cbedd427d978538187
Aadi-0411 <aadityapatil2004@gmail.com>
2025-09-18 12:24:07 +0530

    Added updateProfile Route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -91,5 +92,25 @@ export const logout = (req, res) => {
   }catch(error){
     console.log("Error in logout controller", error.meesage);
     res.status(500).json({message:"Internal Error"});
+  }
+};
+
+export const updateProfile = async(req,res) => {
+  try{
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+
+    if(!profilePic){
+      return res.status(400).json({message:"Profile pic is required"});
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true});
+
+    res.status(200).json(updatedUser);
+  }catch(error){
+    console.log("Error in updating profile: ",error);
+    res.status(500).json({message:"Internal server error"});
   }
 };
